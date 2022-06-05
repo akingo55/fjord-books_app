a66e69ac674daac389f3dfc80708877ccf511d67
akingo55 <am1157154@gmail.com>
2022-06-05 15:51:26 +0900

    add report crud

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.order(:id).page(params[:page])
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @report = Report.find(params[:id])
+
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @report = Report.find(params[:id])
+    if @report.destroy
+      redirect_to @report, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+    else
+      redirect_to @report
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content).merge(user_id: current_user.id)
+  end
+end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  belong_to :user
+  belongs_to :user
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def format_time
+    self.created_at.strftime('%Y/%m/%d %H:%M')
+  end
+
+  def user_name
+    user_id = self.user_id
+    User.find(user_id).name
+  end
 end
