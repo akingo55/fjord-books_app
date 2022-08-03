bf33ee341804136c8cbead9bc0f0d3936f978527
akingo55 <am1157154@gmail.com>
2022-08-03 22:44:29 +0900

    add unit test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'report is editable?' do
+    one = reports(:one)
+    two = reports(:two)
+
+    alice = users(:alice)
+    bob = users(:bob)
+
+    assert one.editable?(alice)
+    assert_not one.editable?(bob)
+
+    assert two.editable?(bob)
+    assert_not two.editable?(alice)
+  end
+
+  test 'format created_on' do
+    one = reports(:one)
+    assert_equal one.created_on, Time.zone.today.to_date
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'alice follows bob' do
+    alice = users(:alice)
+    bob = users(:bob)
+
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+
+    alice.follow(bob)
+
+    assert alice.following?(bob)
+    assert bob.followed_by?(alice)
+
+    alice.unfollow(bob)
+
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+  end
+
+  test 'user name or email' do
+    user = User.new(name: '', email: 'foo@example.com')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'foo'
+    assert_equal 'foo', user.name_or_email
+  end
 end
